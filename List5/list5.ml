eeec5a55afdab0c6ff850a5b27a5cdd05e1aff5c
pmaslankowski <pmaslankowski@gmail.com>
2016-11-27 22:25:59 +0100

    List 5 finished.

--- List5/list5.ml
+++ List5/list5.ml
@@ -33,6 +33,14 @@ let rec ltake l n =
     | LNil -> []
     | LCons (x, lazy xs) -> if n > 0 then x::ltake xs (n-1) else []
 
+let concat_map f l = List.concat (List.map f l) 
+
+let rec lmap f l = 
+  match l with
+    | LNil -> LNil
+    | LCons (x, lazy xs) -> LCons (f x, lazy (lmap f xs))
+
+
 (* exercise 2 *)
 let ltree2llist (tree : 'a ltree) : 'a llist =
   (* linear bfs on given tree *)
@@ -53,3 +61,46 @@ let rec test2 n = LNode (n, LCons (lazy (test2 (n+1)), lazy LNil))
 
 
 (* exercise 3 *)
+let rec rewrite_tree (rules : (string * string) list) (word : string) =
+  (* function returns list consisting all words created by rule *)
+  let rec transform_with (pattern, dest) word =
+    
+    (*function returns list with indexes where pattern starts in a word *)
+    let find word pattern = 
+      let rec loop i j start = 
+        match i, j with
+          | _, _ when i < String.length word && j < String.length pattern ->
+            if   (word.[i]) == (pattern.[j]) 
+            then loop (i+1) (j+1) start
+            else loop (start+1) 0 (start+1)
+          | _, _ when j == String.length pattern -> start::(loop (start+1) 0 (start+1))
+          | _, _ -> []
+      in loop 0 0 0
+    in
+
+    let replace word dest start = String.init (String.length word) (fun i ->  
+      if i < start || i >= start + (String.length dest) 
+      then word.[i] else dest.[i - start])
+    in
+
+    let indexes = find word pattern
+    in List.map (fun x -> replace word dest x) indexes
+  in 
+  let next_words = concat_map (fun (x, y) -> transform_with (x, y) word) rules in
+  let lnext_words = lfromlist next_words in
+  let children = lmap (fun x -> lazy (rewrite_tree rules x)) lnext_words
+  in
+  LNode (word, children)
+
+
+(* exercise 4 *)
+let isReachable rules u u' = 
+  let rec bfs queue = 
+    match queue with
+      | [] -> false
+      | (lazy x)::xs -> match x with
+        | LNode (currWord, children) -> if currWord = u' then true 
+                                        else bfs (xs @ (fromllist children))
+        | LEmpty -> bfs xs
+  in
+  bfs [lazy (rewrite_tree rules u)]
