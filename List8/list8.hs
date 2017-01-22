187175399f19fba81e63e6cba8511aa9d03fa9c0
Piotr Maślankowski <pmaslankowski@gmail.com>
2017-01-22 22:47:13 +0100

    Exercise 2 finished

--- List8/list8.hs
+++ List8/list8.hs
@@ -1,5 +1,8 @@
 import qualified Data.Set as DataSet
+import qualified Data.Map as DataMap
 import Data.Set (Set)
+import Data.Map (Map, (!))
+import Control.Monad
 
 -- Exercise 1:
 newtype OptGraph a = OptGraph (Int, Int -> Set Int, Int -> a, a -> Int)
@@ -61,4 +64,16 @@ print_graph graph =
      sequence [ print $ DataSet.toList $ neighbors graph x | x <- DataSet.toList $ nodes graph]
      return ()
 
-
+get_graph :: Ord a => (String -> a) -> IO (ListGraph a)
+get_graph f = let readEdges x = do neighs <- getLine
+                                   return (x, map f $ read neighs) 
+              in do nodesStr <- getLine
+                    nodes <- return $ map f $ read nodesStr
+                    edgesList <- mapM readEdges nodes
+                    edgesMap <- return $ DataMap.fromList edgesList 
+                    return $ ListGraph (nodes, \x -> edgesMap ! x)
+                    
+-- for testing purposes
+main :: IO ()
+main = get_graph id >>= print_graph
+                 
