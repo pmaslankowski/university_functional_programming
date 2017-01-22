1fcf9cd17989112ad2c04c46bcd4d9f7091b9eae
Piotr Maślankowski <pmaslankowski@gmail.com>
2017-01-22 16:53:53 +0100

    Exercises 1, 2a finished.

---
+++ List8/list8.hs
@@ -0,0 +1,57 @@
+import qualified Data.Set as DataSet
+import Data.Set (Set)
+
+-- Exercise 1:
+newtype OptGraph a = OptGraph (Int, Int -> Set Int, Int -> a, a -> Int)
+newtype ListGraph a = ListGraph ([a], a -> [a])
+
+class Graph g where 
+  nodes :: Ord a => g a -> Set a
+  
+  neighbors :: Ord a => g a -> a -> Set a
+  
+  opt :: Ord a => g a -> OptGraph a
+  opt graph = OptGraph (n, e, l, i)
+    where nodesSet = nodes graph
+          n        = DataSet.size nodesSet
+          l        = \x -> DataSet.elemAt x nodesSet
+          i        = \x -> DataSet.findIndex x nodesSet
+          e        = \x -> DataSet.map i (neighbors graph $ l x)
+  
+  graph_eq :: (Eq a, Ord a) => g a -> g a -> Bool
+  graph_eq g1 g2 = 
+    let nodesList1 = DataSet.toList $ nodes g1
+        nodesList2 = DataSet.toList $ nodes g2
+        edgePairs  = map (\x -> (neighbors g1 x, neighbors g2 x)) nodesList1
+    in (nodes g1 == nodes g2) && all (\(x, y) -> x == y) edgePairs
+
+instance Graph OptGraph where
+  nodes (OptGraph (n, e, l, i)) = DataSet.fromList $ map l [0..n-1]
+
+  neighbors (OptGraph (n, e, l, i)) x = DataSet.map l (e $ i x)
+
+  opt x = x
+
+instance Graph ListGraph where
+  nodes (ListGraph (vs, edges)) = DataSet.fromList vs
+
+  neighbors (ListGraph (vs, edges)) x = DataSet.fromList $ edges x
+
+
+-- TESTS
+test1 = ListGraph ("abcdef", \x -> case x of 'a' -> "bc"
+                                             'b' -> "c"
+                                             'c' -> "df"
+                                             'd' -> "ef"
+                                             'e' -> "a"
+                                             'f' -> "" )
+
+
+-- Exercise 2:
+print_graph :: (Ord a, Show a, Graph g) => g a -> IO ()
+print_graph graph =
+  do print $ nodes graph 
+     sequence [ print $ DataSet.toList $ neighbors graph x | x <- DataSet.toList $ nodes graph]
+     return ()
+
+
